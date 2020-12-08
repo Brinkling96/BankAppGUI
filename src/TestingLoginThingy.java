5935682ea6bdc1ec69646086dec8a45dd47be732
Brinkling96 <67553433+Brinkling96@users.noreply.github.com>
2020-12-08 14:57:02 -0500

    jlkjlk

---
+++ src/TestingLoginThingy.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class TestingLoginThingy {
+    public static void main(String[]  args) {
+        JFrame frame = new JFrame("Test");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        BankUser s = new BankUser("sdbrady","password".toCharArray());
+        s.addCheckingAccount(new CheckingAccount(1000));
+        ArrayList<BankUser> temp = new ArrayList<>();
+        temp.add(s);
+
+        Bank bank = new Bank(temp);
+
+        frame.add(new LoginScreen(frame,bank));
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
