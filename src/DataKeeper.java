69b64473f40c4813490e7eebd2ae3b1f76603f2c
szhou97 <szhou97@bu.edu>
2020-12-13 22:04:57 -0600

    minor output change

--- src/DataKeeper.java
+++ src/DataKeeper.java
@@ -54,7 +54,7 @@ public class DataKeeper {
         try(FileWriter fw = new FileWriter(file, true);
             BufferedWriter bw = new BufferedWriter(fw);
             PrintWriter out = new PrintWriter(bw)) {
-            out.println(transaction.toString());
+            out.print(transaction.toString());
         } catch (IOException e) {
             System.err.println("Transaction could not be created");
 
