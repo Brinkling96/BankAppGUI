26291dfa04eb4227140c0b2aa72f22a5dd3fbf9e
szhou97 <szhou97@bu.edu>
2020-12-13 22:06:43 -0600

    Removed ID suffix

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -2,7 +2,6 @@ public class SavingsAccount extends Account{
 
     public SavingsAccount(Integer balance, User user) {
         super(balance, user);
-        this.accountID = getAccountID() + "sv";
     }
 
 }
