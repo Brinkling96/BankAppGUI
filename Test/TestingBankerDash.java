8796e82433c746218d23433820048c279b0f3a39
szhou97 <szhou97@bu.edu>
2020-12-14 16:30:28 -0600

    Constructor updates

--- Test/TestingBankerDash.java
+++ Test/TestingBankerDash.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 public class TestingBankerDash {
     public static void main(String[]  args) {
         JFrame frame = BankingFrame.getInstance();
-        CustomerUser s = new CustomerUser("sdbrady","password".toCharArray());
-        s.addAccount(new CheckingAccount(1000,s));
+        CustomerUser s = new CustomerUser("sdbrady","password".toCharArray(),0);
+        s.addAccount(new CheckingAccount(1000,s,s.getNumAccounts()));
         ArrayList<User> temp = new ArrayList<>();
         temp.add(s);
 
 
 
-        Banker d = new Banker("banker","pass".toCharArray());
+        Banker d = new Banker("banker","pass".toCharArray(),0);
 
 
         temp.add(d);
