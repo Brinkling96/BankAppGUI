be4daa6f4c311b52510b1cc0365bfdc81ee166b7
szhou97 <szhou97@bu.edu>
2020-12-15 15:50:51 -0600

    Changed parameters

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,17 +1,17 @@
 public class SavingsAccount extends Account{
 
-    public SavingsAccount(Integer balance, User user, int numAccounts) {
+    public SavingsAccount(Integer balance, User user, int numAccounts, Bank bank) {
 
-        super(balance, user, numAccounts);
+        super(balance, user, numAccounts, bank);
     }
 
     public SavingsAccount(String accountID, String balance) {
         super(accountID, balance);
     }
 
-    public void accumulateInterest(Bank bank){
-        if(this.getBalance() >= bank.getHighValueBenchmark()){
-            this.setBalance((int)(this.getBalance()*(1 + bank.getInterestRate())));
+    public void accumulateInterest(){
+        if(this.getBalance() >= this.getBank().getHighValueBenchmark()){
+            this.setBalance((int)(this.getBalance()*(1 + this.getBank().getInterestRate())));
         }
     }
 }
